ed930f6611ab037d85a4de5ef907be0140f93468
Herman Rommetveit <33314755+hermanro77@users.noreply.github.com>
2019-11-28 05:23:56 +0000

    Tests for group 25 (#6)

---
+++ T25-03/program.py
@@ -0,0 +1,5 @@
+ab = "ssof"
+cd = t(ab, vuln)
+ef = s("hack")
+gh = t(ef)
+sink(gh,cd)
---
+++ T25-05/program.py
@@ -0,0 +1,9 @@
+nis=a('nis')
+while indarg == "":
+      query="xpto" + arg3
+      arg3 = arg2
+      arg2 = arg1
+      arg1 = nis
+      indarg = s(indarg,1)
+q=x(query)
+
