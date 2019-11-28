f16f5ff92a17af293e0d5ec20bdc6037be848102
Pedro Bernardo <pedro_bernardo@icloud.com>
2019-11-28 00:28:00 +0000

    group 22 tests

---
+++ T22-06/program.py
@@ -0,0 +1,8 @@
+a = c()
+b = c()
+if a:
+    if True:    
+        b = t(b)
+        w(b)
+    else:
+        w(a)
---
+++ T22-08/program.py
@@ -0,0 +1,8 @@
+if t != '':
+    a = t
+    while a != False and t == True:
+        b = a
+        if b > 3:
+            i = c()
+            b = b and i
+w(b)
